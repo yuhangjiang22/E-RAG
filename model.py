9b4d871a75c27ef0fb95b165af3abf38619f72dd
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-11-11 16:53:06 -0500

    Minor changes.

--- model.py
+++ model.py
@@ -543,18 +543,18 @@ class ERAGWithDocumentMHAttention(PreTrainedModel):
         # logger.info('num_docs: {}'.format(num_docs))
         # batch_size x num_docs x seq_len x hidden_size
         logger.info('doc_sequence_output shape1: {}'.format(doc_sequence_output.shape))
-        doc_sequence_output = doc_sequence_output.view(batch_size, num_docs, seq_len, hidden_size)
+        # doc_sequence_output = doc_sequence_output.view(batch_size, num_docs, seq_len, hidden_size)
         logger.info('doc_sequence_output shape2: {}'.format(doc_sequence_output.shape))
         # logger.info('doc_sequence_output: {}'.format(doc_sequence_output))
 
-        doc_input_mask = doc_input_mask.view(batch_size, num_docs, seq_len)
+        # doc_input_mask = doc_input_mask.view(batch_size, num_docs, seq_len)
         # logger.info('doc_input_mask: {}'.format(doc_input_mask))
         # batch_size x num_docs * seq_len x hidden_size
-        doc_sequence_output = doc_sequence_output.view(batch_size, seq_len * num_docs, hidden_size)
+        # doc_sequence_output = doc_sequence_output.view(batch_size, seq_len * num_docs, hidden_size)
         logger.info('doc_sequence_output shape3: {}'.format(doc_sequence_output.shape))
         # logger.info('doc_sequence_output: {}'.format(doc_sequence_output))
 
-        doc_input_mask = doc_input_mask.view(batch_size, seq_len * num_docs)
+        # doc_input_mask = doc_input_mask.view(batch_size, seq_len * num_docs)
         # logger.info('doc_input_mask: {}'.format(doc_input_mask))
         # Compute the attention-weighted document representation
         attended_doc_rep, attention_probs = self.document_attention(input_cls_rep, doc_sequence_output,
