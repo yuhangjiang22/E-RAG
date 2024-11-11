89c8dfaa4d3888b296a99623fc441e633a5c8233
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-11-11 14:49:05 -0500

    Minor changes.

--- model.py
+++ model.py
@@ -585,6 +585,6 @@ class ERAGWithDocumentMHAttention(PreTrainedModel):
 
             # Total loss (using the dynamic relevance score to weigh input and document losses)
             # loss = dynamic_relevance_score * doc_loss + (1 - dynamic_relevance_score) * input_loss + combined_loss
-            return combined_loss
+            return combined_loss, combined_logits
         else:
             return combined_logits
